3e114d856a99d2f11a30bfea1dee5745d33ad583
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-08-25 17:37:33 +0600

    добавлена функция редактирования инпута

--- imageboard/src/containers/Imageboard/Imageboard.js
+++ imageboard/src/containers/Imageboard/Imageboard.js
@@ -3,6 +3,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {getMessages, postMessages} from "../../store/actions/MessageActions";
 import './Imageboard.css';
 import baseApi from "../../baseApi";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const Imageboard = ({onSubmit}) => {
     const  dispatch = useDispatch();
@@ -13,31 +14,52 @@ const Imageboard = ({onSubmit}) => {
     });
 
     const messages = useSelector(state => state.messages.messages);
+    const loading = useSelector(state => state.messages.loading);
 
     useEffect( ()=> {
         dispatch(getMessages());
     }, [dispatch]);
 
+    const inputChange = e => {
+        const {name,value} = e.target;
+        setData(prev => ({
+            ...prev,
+            [name]: value
+        }));
+    };
+
 
     const makeNewItem = async(e) => {
         e.preventDefault();
+
         const formData = new FormData();
         Object.keys(data).forEach(key => {
             formData.append(key, data[key]);
         });
+
         await dispatch(postMessages(formData));
         dispatch(getMessages());
+    };
+
+    const fileChangeHandler = e => {
+        const name = e.target.name;
+        const file = e.target.files[0];
+        setData(prevState => ({...prevState, [name]: file}));
     };
 
 
     return messages && (
         <>
+
+
             <form   onSubmit={makeNewItem} key='form'>
                 <input
                     placeholder='Message'
                     type='text'
                     required
                     name='message'
+                    value={data.message}
+                    onChange={inputChange}
                     className='messageItem'
                 />
 
@@ -45,12 +67,15 @@ const Imageboard = ({onSubmit}) => {
                     placeholder='Author'
                     type='text'
                     name='author'
+                    value={data.author}
+                    onChange={inputChange}
                     className='authorItem'
                 />
                 <input
                     placeholder='image'
                     type='file'
                     name='image'
+                    onChange={fileChangeHandler}
                 />
 
                 <button type='submit' className='btnPost'>Post item</button>
